deb6207aef97cf04838d70a1883327cd66d6f069
ankikong <ankikong@qq.com>
2020-05-03 12:52:34 +0800

    增加报错提示

--- utility/GetPlayList.py
+++ utility/GetPlayList.py
@@ -2,6 +2,9 @@ import json
 import requests
 import sqlite3
 from utility import tool
+import logging
+
+logger = logging.getLogger("fileLogger")
 
 
 def get_work_list():
@@ -22,34 +25,40 @@ def get_work_list():
         }
         for _ in range(pages):
             url = "https://www.googleapis.com/youtube/v3/playlistItems"
-            _res = s.get(url, params=params, useProxy=True).json()
+            _res: dict = s.get(url, params=params, useProxy=True).json()
+            if _res.get("error") is not None:
+                _res = _res["error"]
+                logger.error("code[{}],message[{}]".format(
+                    _res["code"], _res["message"]))
+                raise Exception("获取视频失败，请检查配置文件setting.yaml")
             for __ in _res["items"]:
                 tmp_data = __["snippet"]
                 video_id = tmp_data["resourceId"]["videoId"]
-                db_res = db.execute("select count(vid) from data where vid='{}';".format(video_id)).fetchone()[0]
+                db_res = db.execute(
+                    "select count(vid) from data where vid='{}';".format(video_id)).fetchone()[0]
                 if int(db_res) != 0:
                     # print(video_id)
                     continue
                 tmpTitle = tmp_data["title"]
                 tmpRs = settings.copy()
                 tmpRs.update({
-                        "title": tmpTitle[0:min(80, len(tmpTitle))],  # 破站限制长度
-                        "id": video_id,
-                        "ptitle": str(settings.get("title", "")).format(title=tmpTitle, 
-                                                                     ctitle=tmp_data["channelTitle"],
-                                                                     ptime=tmp_data["publishedAt"],
-                                                                     surl="https://www.youtube.com/watch?v=" + video_id),
-                        "desc": str(settings.get("desc", "")).format(title=tmpTitle, 
-                                                                     ctitle=tmp_data["channelTitle"],
-                                                                     ptime=tmp_data["publishedAt"],
-                                                                     surl="https://www.youtube.com/watch?v=" + video_id)
+                    "title": tmpTitle[0:min(80, len(tmpTitle))],  # 破站限制长度
+                    "id": video_id,
+                    "ptitle": str(settings.get("title", "")).format(title=tmpTitle,
+                                                                    ctitle=tmp_data["channelTitle"],
+                                                                    ptime=tmp_data["publishedAt"],
+                                                                    surl="https://www.youtube.com/watch?v=" + video_id),
+                    "desc": str(settings.get("desc", "")).format(title=tmpTitle,
+                                                                 ctitle=tmp_data["channelTitle"],
+                                                                 ptime=tmp_data["publishedAt"],
+                                                                 surl="https://www.youtube.com/watch?v=" + video_id)
                 })
                 # tmpRs["tags"] = tmpRs.get("tags", "").split(",")
 
                 ptitle = tmpRs.get("ptitle", "")
                 ptitle = ptitle[0:min(80, len(ptitle))]
                 tmpRs["ptitle"] = ptitle
-                
+
                 desc = tmpRs.get("desc", "")
                 desc = desc.replace("\\n", "\n")
                 desc = desc[0:min(250, len(desc))]
