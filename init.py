dc22a8dab7549a8efa8e4b42432b76a2c7ab40bd
ankikong <ankikong@qq.com>
2020-05-12 21:39:07 +0800

    fix

--- init.py
+++ init.py
@@ -14,13 +14,23 @@ except:
 
 db = sqlite3.connect("data/data.db")
 try:
-    db.execute("""CREATE TABLE data(vid text PRIMARY KEY NOT NULL,
-                                    bvid text,
-                                    zht Boolean DEFAULT 0,
-                                    zhs Boolean DEFAULT 0,
-                                    en Boolean DEFAULT 0, 
-                                    cid text,
-                                    title text);""")
+    db.execute("""
+    create table data (
+            zht Boolean DEFAULT 0,
+            zhs Boolean DEFAULT 0,
+            en Boolean DEFAULT 0,
+            needSubtitle Boolean DEFAULT 1,
+            deleted Boolean DEFAULT 0,
+            stime timestamp DEFAULT 0,
+            subtime timestamp DEFAULT 0,
+            errorcode int DEFAULT 0,
+            vid text PRIMARY KEY NOT NULL,
+            filename text,
+            bvid text,
+            cid text,
+            title text,
+            platform text DEFAULT 'ytb'
+    )""")
 except:
     pass
 
