507fb0f91bc0894d1e9be211fac8a71d6385b80b
HotoCocoaco <42245579+HotoCocoaco@users.noreply.github.com>
2022-11-13 09:00:18 +0800

    I don't know how to py.

--- upload.py
+++ upload.py
@@ -106,7 +106,7 @@ def get_all_video(_config):
 
 
 def download_video(url, out):
-    subprocess.run(["yt-dlp", url, "-o --match-filter !is_live", out], check=True)
+    subprocess.run(["yt-dlp", url, "-o", out, "--match-filter !is_live"], check=True)
 
 
 def download_cover(url, out):
